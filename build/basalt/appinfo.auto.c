cc3634c6e4559d17a3259fa51582c7a36c414f0c
David Svantesson <davidsvantesson@gmail.com>
2015-09-05 19:37:05 +0100

    Build for versin 1.0

---
+++ build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "VastTid",
+  .company = "David Svantesson",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x1F, 0x9A, 0x96, 0xF4, 0x82, 0x2E, 0x4E, 0x06, 0xA2, 0x40, 0xDE, 0x6E, 0x7E, 0x59, 0x97, 0x84 },
+  .virtual_size = 0xb6b6
+};
